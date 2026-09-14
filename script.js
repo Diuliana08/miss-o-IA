d1f889660046c43b38c2e82d7f4ed050fa8548f6
Diuliana08 <gomes.diuliana@escola.pr.gov.br>
2026-09-14 08:13:56 -0300

    aula1

--- script.js
+++ script.js
@@ -10,11 +10,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Isso é assustador!",
-                afirmacao: "afirmacao"
+                afirmacao:[
+                    "No início, ficou com medo do que a tecnologia pode fazer"
+                    "Achou assutador pensar na velocidade com que a tecnologia está avançando"
+                ]    
             },
             {
                 texto: "Isso é maravilhoso!",
-                afirmacao: "afirmacao"
+                afirmacao:[
+                     "Quis saber como usar IA no eu di a dia"
+                     "Pensou que a IA pode ajudar em tarefas da sua vida"
+                ]     
             }           
             
         ]
